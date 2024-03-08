65c103a43e46399a4c63e0d57c920b19fb0c6c17
P1587 <P1587@PT-A154401874>
2024-03-08 14:52:24 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CreateMVVMProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnLoadPath = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLoadPath = new System.Windows.Forms.TextBox();
+            this.txtProjectName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(567, 73);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "送出";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // btnLoadPath
+            // 
+            this.btnLoadPath.Location = new System.Drawing.Point(567, 28);
+            this.btnLoadPath.Name = "btnLoadPath";
+            this.btnLoadPath.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadPath.TabIndex = 1;
+            this.btnLoadPath.Text = "...";
+            this.btnLoadPath.UseVisualStyleBackColor = true;
+            this.btnLoadPath.Click += new System.EventHandler(this.BtnLoadPath_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "建立路徑";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "專案名稱";
+            // 
+            // txtLoadPath
+            // 
+            this.txtLoadPath.Location = new System.Drawing.Point(82, 25);
+            this.txtLoadPath.Name = "txtLoadPath";
+            this.txtLoadPath.Size = new System.Drawing.Size(466, 22);
+            this.txtLoadPath.TabIndex = 4;
+            // 
+            // txtProjectName
+            // 
+            this.txtProjectName.Location = new System.Drawing.Point(82, 70);
+            this.txtProjectName.Name = "txtProjectName";
+            this.txtProjectName.Size = new System.Drawing.Size(240, 22);
+            this.txtProjectName.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 110);
+            this.Controls.Add(this.txtProjectName);
+            this.Controls.Add(this.txtLoadPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLoadPath);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnLoadPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLoadPath;
+        private System.Windows.Forms.TextBox txtProjectName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateMVVMProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLoadPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog
+            {
+                ShowNewFolderButton = true
+            };
+            dialog.ShowDialog();
+
+            txtLoadPath.Text = dialog.SelectedPath;
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtLoadPath.Text) && !string.IsNullOrEmpty(txtProjectName.Text))
+            {
+                string ProjectPath = string.Format(@"{0}\{1}", txtLoadPath.Text, txtProjectName.Text);
+                if (Directory.Exists(ProjectPath))
+                {
+                    MessageBox.Show("專案已存在");
+                }
+                else
+                {
+                    Directory.CreateDirectory(ProjectPath);
+                    string ExamplePath = ConfigurationManager.AppSettings["ExampleProjectPath"];
+                    CreateLevel1(ExamplePath, ProjectPath); //改專案名
+                    // CreateLevel2(string.Format(@"{0}\ReportExample", ExamplePath), string.Format(@"{0}\{1}", ProjectPath,txtProjectName.Text));
+                    CreateLevel2(string.Format(@"{0}\WpfMVVMExample", ExamplePath), string.Format(@"{0}\{1}", ProjectPath,txtProjectName.Text));
+                    // CreateLevel3Properties(string.Format(@"{0}\ReportExample\Properties", ExamplePath), string.Format(@"{0}\{1}\Properties", ProjectPath, txtProjectName.Text));
+                    CreateLevel3Properties(string.Format(@"{0}\WpfMVVMExample\Properties", ExamplePath), string.Format(@"{0}\{1}\Properties", ProjectPath, txtProjectName.Text));
+                    // CreateLevel3ReportInfo(string.Format(@"{0}\ReportExample\ReportInfo", ExamplePath), string.Format(@"{0}\{1}\ReportInfo", ProjectPath, txtProjectName.Text));
+
+                    MessageBox.Show("新增完畢");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("未填寫完成");
+            }
+
+        }
+
+
+        private void CreateLevel1(string ReadPath, string CreatePath)
+        {
+            Directory.CreateDirectory(string.Format(@"{0}\{1}", CreatePath, txtProjectName.Text));
+
+            //   CreateReplceFile(ReadPath, CreatePath, "ReportExample.sln", txtProjectName.Text + ".sln");
+            CreateReplceFile(ReadPath, CreatePath, "WpfMVVMExample.sln", txtProjectName.Text + ".sln");
+        }
+
+        private void CreateLevel2(string ReadPath, string CreatePath)
+        {
+
+            CreateReplceFile(ReadPath, CreatePath, "App.config");
+            Directory.CreateDirectory(string.Format(@"{0}\Properties", CreatePath));
+            //Directory.CreateDirectory(string.Format(@"{0}\ReportInfo", CreatePath));
+            //
+            // CreateReplceFile(ReadPath, CreatePath, "Program.cs");
+            // CreateReplceFile(ReadPath, CreatePath, "REPORT.cs")
+            
+            // CreateReplceFile(ReadPath, CreatePath, "ReportExample.csproj", txtProjectName.Text + ".csproj");
+            CreateReplceFile(ReadPath, CreatePath, "WpfMVVMExample.csproj", txtProjectName.Text + ".csproj");
+
+
+            CreateReplceFile(ReadPath, CreatePath, "MainWindow.xaml.cs");
+            CreateReplceFile(ReadPath, CreatePath, "MainWindow.xaml");
+            CreateReplceFile(ReadPath, CreatePath, "App.xaml.cs");
+            CreateReplceFile(ReadPath, CreatePath, "App.xaml");
+            
+            
+            CreateReplceFile(ReadPath, CreatePath, "MainViewModel.cs");
+            CreateReplceFile(ReadPath, CreatePath, "ViewModelBase.cs");
+            CreateReplceFile(ReadPath, CreatePath, "RelayCommand.cs");
+
+        }
+
+        private void CreateLevel3Properties(string ReadPath, string CreatePath)
+        {
+            CreateReplceFile(ReadPath, CreatePath, "AssemblyInfo.cs");
+
+            CreateReplceFile(ReadPath, CreatePath, "Resources.Designer.cs");
+            CreateReplceFile(ReadPath, CreatePath, "Resources.resx");
+            CreateReplceFile(ReadPath, CreatePath, "Settings.Designer.cs");
+            CreateReplceFile(ReadPath, CreatePath, "Settings.settings");
+        }
+
+        //private void CreateLevel3ReportInfo(string ReadPath, string CreatePath)
+        //{
+        //    CreateReplceFile(ReadPath, CreatePath, "BlockInfo.cs");
+        //    CreateReplceFile(ReadPath, CreatePath, "ConditionInfo.cs");
+        //    CreateReplceFile(ReadPath, CreatePath, "LayoutInfo.cs");
+        //    CreateReplceFile(ReadPath, CreatePath, "UrlLinkInfo.cs");
+        //}
+
+
+        private void CreateReplceFile(string ReadPath, string CreatePath, string FileName)
+        {
+            //寫到檔案裡面更改
+            foreach (var str in File.ReadAllLines(string.Format(@"{0}\{1}", ReadPath, FileName)))
+            {
+                File.AppendAllText(string.Format(@"{0}\{1}", CreatePath, FileName), str.Replace("WpfMVVMExample", txtProjectName.Text) + "\r\n");
+            }
+        }
+        private void CreateReplceFile(string ReadPath, string CreatePath, string FileName, string NewName)
+        {
+            foreach (var str in File.ReadAllLines(string.Format(@"{0}\{1}", ReadPath, FileName)))
+            {
+                File.AppendAllText(string.Format(@"{0}\{1}", CreatePath, NewName), str.Replace("WpfMVVMExample", txtProjectName.Text) + "\r\n");
+                //自己範例的專案名
+            }
+        }
+
+    }
+}
